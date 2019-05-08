ad7fb6992ae935f07522b32f088b85e7e8ea50d5
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-05-08 16:25:41 -0300

    Update qrCode.js

--- app/client/templates/views/modals/qrCode.js
+++ app/client/templates/views/modals/qrCode.js
@@ -7,7 +7,7 @@ The template to display qrCode.
 
 Template['views_modals_qrCode'].onRendered(function(){
     if(this.data && this.data.address) {
-        var qrcodesvg = new Qrcodesvg("ethereum:" + this.data.address, 'qrcode', 150, {"ecclevel" : 1});
+        var qrcodesvg = new Qrcodesvg("puffscoin:" + this.data.address, 'qrcode', 150, {"ecclevel" : 1});
         qrcodesvg.draw({"method": "classic", "fill-colors":["#555","#555","#666"]}, {"stroke-width":1});
     }
 });
